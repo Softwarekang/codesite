b6921fa5757a8a92a8ebe94ddb9119f90916d4b0
softwarekang <jingyechenfu@aliyun.com>
2023-03-24 16:49:40 +0800

    feat(http2): add http2 example

--- http2/client.go
+++ http2/client.go
@@ -3,13 +3,12 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"golang.org/x/net/http2"
 	"net/http"
 )
 
 func main() {
 	// 创建一个HTTP/2客户端传输配置
-	config := &http2.Transport{
+	config := &http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true,
 		},
@@ -29,7 +28,6 @@ func main() {
 	// 读取响应并关闭响应体
 	body := make([]byte, resp.ContentLength)
 	resp.Body.Read(body)
-	resp.Body.Close()
 
 	fmt.Println(string(body))
 }
