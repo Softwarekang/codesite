d8f7592533132675fce2cfeb28d682cd224ec284
softwarekang <jingyechenfu@aliyun.com>
2023-04-15 18:26:41 +0800

    fix(time): change time location to CST

--- time/main.go
+++ time/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	yesterday := time.Now().AddDate(0, 0, -1) // 获取昨天的日期
+	var CST = time.FixedZone("CST", 3600*8)
+	yesterday := time.Now().AddDate(0, 0, -1).In(CST) // 获取昨天的日期
 	yesterdayStart := time.Date(yesterday.Year(), yesterday.Month(), yesterday.Day(), 0, 0, 0, 0, yesterday.Location())
 	for i := 1; i < 25; i++ {
 		rounded := yesterdayStart.Add(time.Hour * time.Duration(i))
