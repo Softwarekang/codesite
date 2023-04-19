ce96a2023c2b80ecbba763a4488e18c5bdadb6d1
softwarekang <jingyechenfu@aliyun.com>
2023-04-19 16:48:40 +0800

    feat(sync): add sync map example

--- sync/unsafe.go
+++ sync/unsafe.go
@@ -8,7 +8,8 @@ import (
 func main() {
 	m := sync.Map{}
 	m.Store(1, 2)
-	m.Store(1, 2)
+	m.Store(1, 3)
+	m.Store(12, 2)
 	m.Range(func(key, value any) bool {
 		fmt.Printf("k:%v, v:%v", key, value)
 		return true
