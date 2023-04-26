cc75e45ac2e109a96949c56f7767e3c0cab5a433
Phoenix <jingyechenfu@aliyun.com>
2023-04-26 22:34:46 +0800

    reflect example

--- reflect/silce.go
+++ reflect/silce.go
@@ -1,14 +1,28 @@
 package main
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-
+	strs := []string{"a", "b"}
+	interfaceSlice := convertToInterfaceSlice(&strs)
+	for _, v := range interfaceSlice {
+		fmt.Println(v)
+	}
 }
 
 // 其他类型 slice 转化为 []interface
 func convertToInterfaceSlice(slice interface{}) []interface{} {
-	sliceValue := reflect.ValueOf(slice)
+	var sliceValue reflect.Value
+	if reflect.TypeOf(slice).Kind() == reflect.Ptr && reflect.TypeOf(slice).Elem().Kind() == reflect.Slice {
+		sliceValue = reflect.ValueOf(slice).Elem()
+	}
+
+	if reflect.TypeOf(slice).Kind() == reflect.Slice {
+		sliceValue = reflect.ValueOf(slice)
+	}
 	result := make([]interface{}, sliceValue.Len())
 	for i := 0; i < sliceValue.Len(); i++ {
 		elemValue := sliceValue.Index(i)
