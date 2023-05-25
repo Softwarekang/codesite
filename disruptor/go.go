0781b805dd3dfaeabcb6556bb31c5216d6ab9dcb
softwarekang <jingyechenfu@aliyun.com>
2023-05-25 21:55:06 +0800

    test(disruptor)

--- disruptor/go.go
+++ disruptor/go.go
@@ -1,4 +1,32 @@
-package disruptor
+package main
 
-type Disruptor struct {
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var arr [][8]int64
+	arr = make([][8]int64, 1024*1024)
+	for i := 0; i < 1024*1024; i++ {
+		for j := 0; j < 8; j++ {
+			arr[i][j] = 1
+		}
+	}
+
+	marked := time.Now().UnixMicro()
+	for i := 0; i < 1024*1024; i++ {
+		for j := 0; j < 8; j++ {
+			arr[i][j] = 2
+		}
+	}
+	fmt.Println("Loop times:", time.Now().UnixMicro()-marked, "ms")
+
+	marked = time.Now().UnixMicro()
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 1024*1024; j++ {
+			arr[j][i] = 2
+		}
+	}
+	fmt.Println("Loop times:", time.Now().UnixMicro()-marked, "ms")
 }
--- time/main.go
+++ time/main.go
@@ -7,14 +7,9 @@ import (
 
 func main() {
 	var CST = time.FixedZone("CST", 3600*8)
-	yesterday := time.Now().AddDate(0, 0, -1).In(CST) // 获取昨天的日期
-	yesterdayStart := time.Date(yesterday.Year(), yesterday.Month(), yesterday.Day(), 0, 0, 0, 0, yesterday.Location())
-	for i := 1; i < 25; i++ {
-		rounded := yesterdayStart.Add(time.Hour * time.Duration(i))
-		fmt.Println(rounded.Hour() + i)
-		timestamp := rounded.Unix()
-		fmt.Println(timestamp)
-	}
+
+	date := time.Date(2023, time.February, 40, 0, 0, 0, 0, CST)
+	fmt.Printf(date.String())
 }
 
 // 1680058800
